ed8783a719a72da308b78415419abb0eeb7967be
frankmenglong <menglong.yuan@hotmail.com>
2017-02-28 01:05:37 +0100

    add weather js

--- watchface/src/c/watchface.c
+++ watchface/src/c/watchface.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
-
+#define TEMPERATURE 0
+#define CONDITIONS 1
 
 // Declare globally
 static GFont s_time_font;
@@ -11,6 +12,45 @@ static GBitmap *s_background_bitmap;
 
 static TextLayer *s_weather_layer;
 static GFont s_weather_font;
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, CONDITIONS);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+
+
+  }
+
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+
+
 
 
 static void update_time() {
@@ -29,6 +69,20 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
+  
 
 }
 
@@ -106,7 +160,22 @@ static void main_window_unload(Window *window) {
 
 }
 
+
+
 static void init(){
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+
+  // Open AppMessage
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
+
+
   // Create main Window element and assign to pointer
   s_main_window = window_create();
 
